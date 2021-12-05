05a11b3fd4f9baadb32b9bcfda671519be11f150
EC2 Default User <ec2-user@ip-172-31-87-210.ec2.internal>
2021-12-05 16:43:00 +0000

    create code

---
+++ first_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+name = input("what is your name?")
+Password = input("what is the password?")
+if name == "Ethan" and Password == "Elephant":
+    print("welcome back, Ethan!")
+else:
+    print("invalid user credentials, access denied")
